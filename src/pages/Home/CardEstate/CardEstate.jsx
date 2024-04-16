8c39f9149ece9a121e73b0278a067008ca8c197c
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-04-16 10:16:32 +0600

    added dynamic card estate

--- src/pages/Home/CardEstate/CardEstate.jsx
+++ src/pages/Home/CardEstate/CardEstate.jsx
@@ -1,67 +1,33 @@
+import { CiLocationOn } from "react-icons/ci";
+import { BsHouseDoor } from "react-icons/bs";
+import { BsArrowUpRightSquare } from "react-icons/bs";
 
-const CardEstate = () => {
+
+const CardEstate = ({estate}) => {
+
+    const {id,estate_title,segment_name,description,price,status,area,location,facilities,image} = estate
+
     return (
-        <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className="duration-300 hover:scale-110 " src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="mt-10  ">
+            
+            <div className="card w-full bg-base-100 shadow-xl">
+                <figure><img className="duration-300 hover:scale-110 " src={image} alt="Estate" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <h2 className="card-title text-2xl">{estate_title}</h2>
+                    <div className="flex my-3">
+                        <p className="text-xl flex gap-1"><CiLocationOn className="mt-1" />{location}</p>
+                        <p className="px-4 rounded-xl text-white text-xl bg-green-500 text-center">{status}</p>
+                    </div>
+                    <div className="flex my-3">
+                    <p className="text-xl flex gap-1"><BsHouseDoor className="mt-1" />{segment_name}</p>
+                    <p className="text-xl pl-8 lg:pl-24 flex gap-1"><BsArrowUpRightSquare className="mt-1"/>{area}</p>
+                    </div>
+                    <div className="card-actions justify-start">
+                        <button className="btn btn-primary">View Details</button>
                     </div>
                 </div>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className="duration-300 hover:scale-110 " src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className="duration-300 hover:scale-110 " src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className="duration-300 hover:scale-110 " src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className="duration-300 hover:scale-110 " src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className="duration-300 hover:scale-110 " src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+           
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,41 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "./Banner/Banner";
 import CardEstate from "./CardEstate/CardEstate";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+
+    const estates = useLoaderData()
     return (
         <div>
             <Helmet>
-        <title>Home | Cotage House</title>
-    </Helmet>
-            <div className="max-w-7xl mx-auto rounded-xl p-5">
-            <Banner></Banner>
-            <CardEstate></CardEstate>
+                <title>Home | Cotage House</title>
+            </Helmet>
+
+            <div className="max-w-6xl mx-auto text-center">
+                <h1 className="text-xl font-bold p-10">
+                    Indulge in luxury and comfort at our curated estates,
+                    Where every stay is a memorable escape.
+                    Discover breathtaking views and exceptional service,
+                    Your ultimate retreat begins here.
+                </h1>
+            </div>
+
+            <div className="max-w-7xl mx-auto rounded-xl">
+                <Banner></Banner>
+            </div>
+            <div className="max-w-7xl mx-auto text-center mt-10">
+            <p className="text-[#5ba600] text-md">we are offering the best real state</p>
+            <h1 className="text-6xl font-bold">Our Properties</h1>
+            </div>
+            <div className="max-w-7xl mx-auto rounded-xl p-5 gap-5 grid lg:grid-cols-3">
+                
+                {
+                    estates.map(estate => <CardEstate
+                        key={estate.id}
+                        estate={estate}
+                    ></CardEstate>)
+                }
             </div>
         </div>
     );
